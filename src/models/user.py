320761b43f6400c6b4a5df1ef3606ad78812e55c
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-18 14:47:22 +0200

    update: User model for users table

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String, Enum, DateTime
+from sqlalchemy import Column, String, Enum, DateTime, Boolean
 from sqlalchemy.sql import func
 from src.models.base import Base
 from src.models.enums import Roles
@@ -11,9 +11,10 @@ class User(Base):
     id = Column(UUID, primary_key=True, index=True)
     username = Column(String(50), unique=True, nullable=False)
     email = Column(String(320), unique=True, nullable=False)
+    password_hash = Column(String(60), nullable=False) # bcrypt hashed password is 60 characters long
     role = Column(Enum(Roles), nullable=False, default=Roles.USER)
     avatar = Column(String(2048), nullable=True)
-    password_hash = Column(String(50), nullable=False) # TODO: check how long should it be
-    created_at = Column(DateTime(timezone=True), server_default=func.now()) # TODO: research what is server_default
-    last_login = Column(DateTime(timezone=True), nullable=True) # TODO: why is this nullable?
+    created_at = Column(DateTime(timezone=True), server_default=func.now()) # can be used for future achievements and badges
+    last_login = Column(DateTime(timezone=True), nullable=True)
+    is_verified = Column(Boolean, nullable=False, default=False)
 
