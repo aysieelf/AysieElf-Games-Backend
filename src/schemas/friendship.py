75a50d76753f7fc4b67467b801b7264ce386dbc4
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-18 17:10:47 +0200

    add: FriendshipCreate pydantic schema

---
+++ src/schemas/friendship.py
@@ -0,0 +1,9 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+class BaseConfig(BaseModel):
+    model_config = {"from_attributes": True}
+
+class FriendshipCreate(BaseConfig):
+    friend_id: UUID
