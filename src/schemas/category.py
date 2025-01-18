09d42c505b3fca050f23039bd7fcd824e3a9e3f1
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-18 16:49:41 +0200

    add: CategoryCreate pydantic schema

---
+++ src/schemas/category.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+# Base configs
+class BaseConfig(BaseModel):
+    model_config = {"from_attributes": True}
+
+class CategoryCreate(BaseConfig):
+    name: str
+    description: Optional[str] = None
