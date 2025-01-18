ae8e64ab7d3df11d965af683b17039ad402ab7a8
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-18 16:24:43 +0200

    add: GameCreate Pydantic schema

---
+++ src/schemas/game.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+# Base configs
+class BaseConfig(BaseModel):
+    model_config = {"from_attributes": True}
+
+class GameCreate(BaseConfig):
+    title: str
+    icon: Optional[str] = None
+    description: Optional[str] = None
+    is_multiplayer: bool = False
+    category_id: UUID
