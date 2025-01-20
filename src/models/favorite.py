9b903911cf251bfd333f6cc6bd51c22b1369ab96
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-20 21:02:29 +0200

    add: Favorite Base Model

---
+++ src/models/favorite.py
@@ -0,0 +1,20 @@
+from src.models.base import Base
+
+from sqlalchemy import TIMESTAMP, UUID, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+
+class Favorite(Base):
+    __tablename__ = "favorites"
+
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True)
+    game_id = Column(UUID(as_uuid=True), ForeignKey("games.id"), primary_key=True)
+    added_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+
+    user = relationship(
+        "User", back_populates="favorites"
+    )
+    game = relationship(
+        "Game", back_populates="favorites"
+    )
