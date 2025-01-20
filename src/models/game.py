617fa1b658c72a9bf22a7ce9f3306b3ea4b07862
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-20 21:02:52 +0200

    update: Game Base Model - add relationship with favorites

--- src/models/game.py
+++ src/models/game.py
@@ -31,6 +31,7 @@ class Game(Base):
     category = relationship("Category", back_populates="games")
     game_activities = relationship("GameActivity", back_populates="game")
     upvotes = relationship("Upvote", back_populates="game")
+    favorites = relationship("Favorite", back_populates="game")
 
     @staticmethod
     def generate_slug(title: str) -> str:
