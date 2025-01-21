1f9d88714e623bff8b43d0e42827e924063e63b0
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-21 14:38:32 +0200

    update: env.py

--- alembic/env.py
+++ alembic/env.py
@@ -68,7 +68,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online():
-    connectable = engine_from_config(
+    engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         url=settings.DATABASE_URL,
