5626d028104c8b0927cdfd696133b1c84ac3b930
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-22 14:38:44 +0200

    update: routes.py - add auth router

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -1,7 +1,8 @@
-from src.api.v1.endpoints import health
+from src.api.v1.endpoints import health, auth
 
 from fastapi import APIRouter
 
 api_router = APIRouter()
 
 api_router.include_router(health.router, tags=["health"])
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
