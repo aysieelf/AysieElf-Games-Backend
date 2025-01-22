b047c538d1fad5817a921f1f3cd6adf66fd83e6a
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-22 14:39:25 +0200

    add: BlacklistedToken Base Model

---
+++ src/models/blacklisted_token.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, TIMESTAMP
+from src.models.base import Base
+from sqlalchemy.sql import func
+
+
+class BlacklistedToken(Base):
+    __tablename__ = "blacklisted_tokens"
+
+    token = Column(String, primary_key=True, index=True)
+    blacklisted_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
+    expires_at = Column(TIMESTAMP(timezone=True), nullable=False)
