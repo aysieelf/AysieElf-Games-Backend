6ca5a1c6cb6c6da3139701ea373d0909ac5ab0e9
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-22 14:38:12 +0200

    update: alembic revision - add new table

---
+++ alembic/versions/bfdb45635537_add_new_table.py
@@ -0,0 +1,26 @@
+"""add new table
+
+Revision ID: bfdb45635537
+Revises: 89db94858bf6
+Create Date: 2025-01-22 14:35:10.172065
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'bfdb45635537'
+down_revision: Union[str, None] = '89db94858bf6'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
