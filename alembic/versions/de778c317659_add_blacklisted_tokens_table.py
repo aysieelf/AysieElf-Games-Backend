de281e3ab786a0664a651670ba31ed1861254ebe
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-22 15:03:23 +0200

    add: alembic upgrade

--- alembic/versions/de778c317659_add_blacklisted_tokens_table.py
+++ alembic/versions/de778c317659_add_blacklisted_tokens_table.py
@@ -8,6 +8,9 @@ Create Date: 2025-01-22 14:35:10.172065
 
 from typing import Sequence, Union
 
+from alembic import op
+import sqlalchemy as sa
+
 # revision identifiers, used by Alembic.
 revision: str = "bfdb45635537"
 down_revision: Union[str, None] = "89db94858bf6"
