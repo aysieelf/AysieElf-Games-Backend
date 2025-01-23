22acaef71775f08c018929e438c57b52832b097f
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-23 08:26:32 +0200

    update: crud user - create_user()

--- src/crud/user.py
+++ src/crud/user.py
@@ -32,3 +32,44 @@ def get_by_id(db: Session, user_id: str) -> Optional[UserReadSingle]:
 
     return UserReadSingle.model_validate(db_user)
 
+
+def create_user(user: UserCreate, db: Session) -> UserReadSingle:
+    """
+    Create a new user with the provided data.
+
+    Args:
+        user (UserCreate): The user data to create.
+        db (Session): The database session.
+
+    Returns:
+        User: The created user object.
+    """
+    v.user_email_exists(db, user.email)
+
+    hashed_password = get_password_hash(user.password)
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        password_hash=hashed_password,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    # send_email_notification(
+    #     email=user.email,
+    #     subject="Account Created",
+    #     message=f"Your account has been created with email {user.email}",
+    # )
+    return UserReadSingle(
+        id=db_user.id,
+        username=db_user.username,
+        slug=db_user.slug,
+        email=db_user.email,
+        role=db_user.role,
+        avatar=db_user.avatar,
+        last_login=db_user.last_login,
+        favorite_games=[],
+        friends=[],
+        game_activities=[],
+    )
