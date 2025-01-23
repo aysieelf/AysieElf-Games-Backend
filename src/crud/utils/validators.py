f786c6f616874498c6a670981677ac7368b9b4bc
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-23 08:27:52 +0200

    add: validators.py - user_email_exists()

---
+++ src/crud/utils/validators.py
@@ -0,0 +1,23 @@
+from src.models.user import User
+
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from fastapi import status
+
+
+def user_email_exists(db: Session, email: str):
+    """
+    Check if an email address already exists in the database.
+
+    Args:
+        db (Session): The database session.
+        email (str): The email address to check.
+
+    Raises:
+        HTTPException: If the email address already exists.
+    """
+    if db.query(User).filter(User.email == email).first():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="There is a user registered with this email address.",
+        )
