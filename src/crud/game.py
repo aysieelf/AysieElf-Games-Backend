bb857a96acd6fc135385339f80dc3d200e6fddba
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-24 17:58:15 +0200

    add: _convert_to_game_read_all() method

---
+++ src/crud/game.py
@@ -0,0 +1,16 @@
+from unicodedata import category
+
+from src.models import Game
+from src.schemas.game import GameReadAll
+
+
+def _convert_to_game_read_all(game: Game) -> GameReadAll:
+    return GameReadAll(
+        id=game.id,
+        title=game.title,
+        slug=game.slug,
+        icon=game.icon,
+        description=game.description,
+        category_slug=game.category.slug,
+        category_title=game.category.title,
+    )
